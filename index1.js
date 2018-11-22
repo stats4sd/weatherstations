834292771d5ee1d11820ef5ff196ffc781ce6cbb
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2018-11-22 17:03:30 +0000

    first part of complete code

--- index1.js
+++ index1.js
@@ -7,11 +7,15 @@ const d3 = require('d3-dsv')
 
 var mysql = require('mysql');
 
-var con = mysql.createPool({
+const math = require('mathjs')
+
+const config = require('./config')
+
+var con = mysql.createConnection({
     host: "localhost",
-    user: "root",
-    password: "Logoslogos88",
-    database: "umsa"
+    user: config.config.username,
+    password: config.config.password,
+    database: config.config.database
 });
 
 
@@ -20,12 +24,17 @@ var con = mysql.createPool({
 // *****************************************************
 
 
+
+
+
 // reads data from file
 // Requires:
 //  - path: path to file (string)
+
+
 async function read(path) {
     return new Promise((resolve, reject) => {
-        fs.readFile(path, "utf16le", (err, data) => {
+        fs.readFile(path, "utf16le", (err, data) => { // utf16le or utf8
             if (err){
                 throw err;
             }
@@ -55,143 +64,236 @@ async function write(path,content) {
 async function main() {
 
     // const path = "./_data/Datos estacion Calahuancane.csv";
-    const path = "C:/Users/LuciaFalcinelli/Documents/GitHub/weatherstations/_data/MODChinchaya 11_10_2018.csv"
+    const path = "C:/Users/LuciaFalcinelli/Documents/GitHub/weatherstations/_data/Chinchaya (14_09_2018).csv"
 
     const rawData = await read(path);
 
-   // console.log("rawData",rawData)
-    //return; 
 
-    let parsedData = d3.tsvParse(rawData);
+
+    //check comma or space
+    let parsedData = d3.tsvParse(rawData); // csv or tsc
+
+
+
+    let numberOne = [];
+    let numberTwo = [];
+    let dateArray = [];
+    let dates = [];
+    let isoDates = [];
+
+    // check if file is from Davis(True) or China(False) Station
+
+    let typeOfStation = true
+    let countColumn = Object.keys(parsedData[0]).length
+
+
+    if(countColumn > 40 ){
+
+        typeOfStation = true ;
+
+         }else{
+
+            typeOfStation = false;
+        
+    }
+
+    //creates column Fecha/Hora in file davis
+
+    if(typeOfStation){
+        parsedData = parsedData.map( (item, index) => {
+           
+        const date = item["Date"];
+        const hour = item["Time"];
+ 
+        item["Fecha/Hora"] = date + " " + hour;
+
+
+        return item
+        
+        })
+    }
+
+
+
+
+
+   // console.log(parsedData)
 
     parsedData = parsedData.map( (item,index) => {
 
-        const date = item["Fecha/Hora"]
 
-        console.log( date );
+        Object.keys(item).forEach((key,i) => {
+            
 
-    //     // hack for now - assume WHATEVER date comes in is "GMT"
-        const parsedDate = new Date(date+" GMT")
-        parsedDate.getFullYear();
-
-        console.log(parsedDate);
-
-
-         item["Timestamp"] = parsedDate.toISOString();
+            //remove excess properties
+            if(key.trim() != key) {
+                item[key.trim()] = item[key];
+                delete item[key];
 
 
-    //     // rename headers from Chinas stations
-    //    // item["temperatura_interna"] = item["Temperatura Interna(°C)"];
-    //     //delete item["Temperatura Interna(°C)"];
+            }
+        })
+        
+        const date = item["Fecha/Hora"];
+      //  console.log(date)
 
-         delete item["Fecha/Hora"]
+        // check format of date
+        // let dateArray = [];
+        // check if '/' or '-' exist and create an array with the following elements: dd, mm, yyyy hh:mm:ss
+        if(date.indexOf('/') > -1) {
+            dateArray = date.split('/');
+        }
+        else if(date.indexOf('-') > -1){
+            dateArray = date.split('-');
+        }
 
-         return item
-     })
+        
 
+
+        // replace a.m. and p.m. with AM and PM
+           
+        dateArray[2] = dateArray[2].replace("a.m.", "AM")
+        dateArray[2] = dateArray[2].replace("p.m.", "PM")
+
+        // remove whitespace from both sides of a string
+
+        dates.push(dateArray[0].trim())
+        dates.push(dateArray[1].trim())
+        dates.push(dateArray[2].trim())
+
+        // create two arrays with days and months of dateArray   
+
+        numberOne.push(Number(dateArray[0]))
+        numberTwo.push(Number(dateArray[1]))
+        //console.log("number 1 ",numberOne)
+        //console.log("number 2 ",numberTwo)
+
+        return item
+    })
+
+    //calculate standard deviation between months and days 
+
+    var numOneStd = math.std(numberOne);
+    var numTwoStd = math.std(numberTwo);
+  
+    //console.log(numTwoStd)
+    //console.log(numOneStd)
+
+    // returns a string with the following order: mm, dd, yyyy hh:mm:ss 
+    for (var i = 0; i <= dates.length - 1; i += 3) {
+
+        if (numOneStd > numTwoStd ) {
+            date = dates[i]
+            dates[i] = dates[i+1]
+            dates[i+1] = date
+            dates[i+2] = dates[i+2]            
+
+        }else if(numOneStd < numTwoStd){
+            dates[i] = dates[i]
+            dates[i+1] = dates[i+1]
+            dates[i+2] = dates[i+2]
+
+
+        }
+
+ 
+    } 
+
+
+
+      // check month and hour length and adds 0 where it is missing 
+
+        parsedData = parsedData.map((item, index) => {
+           // str = ""
+            str = (dates[index])
+
+            length = str.length
+            //console.log("length", length)
+           
+            if(length == 1){
+                dates[index] = "0" + dates[index]
+            } else if (length == 15){
+                dates[index] = dates[index].substring(0, 5) + "0" + dates[index].substring(5, 15)
+
+
+            }
+
+
+            return item
+
+        })
+        
+
+        
+        // returns an arrays with dates in ISO format (yyyy-mm-dd hh:mm:ss)  
+        for (var i = 0; i <= dates.length - 1; i += 3) {
+
+            let dateString =dates[i] + "-" + dates[i+1] + "-" + dates[i+2] + " GMT";
+        
+            const parsedDate = new Date(dateString)
+            dateString = parsedDate.toISOString();
+            dateString = dateString.replace("T"," ")
+            dateString = dateString.replace("Z", "")
+            isoDates.push(dateString)
+
+
+    }
+
+    
+        // substitute ISO format dates in the Fecha/Hora column  
+        parsedData = parsedData.map((item, index) =>{
+
+            item["Fecha/Hora"] = isoDates[index] 
+    
+            return item
+    
+    })
+
+
+
+
+    // create new file with new dates
 
     const outData = d3.csvFormat(parsedData);
-    const newPath = "./_data/Chinchaya(11_10_2018) - Dates.csv"
+
+    const newPath = "./_data/Dates.csv"
 
     write(newPath, outData);
 
-   // console.log("parsed data is ", JSON.stringify(parsedData) )
-
-    con.getConnection((err, connection) => {
-        //test with just intervalo 
-        parsedData = parsedData.map( (item, index) => {
-            var newItem = {}
-            newItem['intervalo'] = item['Intervalo'];
-            newItem['fecha_hora'] = item['Fecha/Hora'];
-            newItem['temperatura_interna'] = item['Temperatura Interna(°C)'];
-            newItem['humedad_interna'] = item['Humedad Interna(%)'];
-            newItem['temperatura_externa'] = item['Temperatura Externa(°C)'];
-            newItem['humedad_externa'] = item['Humedad Externa(%)'];
-            newItem['presion_relativa'] = item['Presión Relativa(hpa)'];
-            newItem['presion_absoluta'] = item['Presión Absoluta(hpa)'];
-            newItem['velocidad_viento'] = item['Velocidad del viento(m/s)'];
-            newItem['sensacion_termica'] = item['Sensación Térmica(°C)'];
-            newItem['rafaga'] = item['Ráfaga(m/s)'];
-            newItem['direccion_del_viento'] = item['Dirección del viento'];
-            newItem['punto_rocio'] = item['Punto de Rocío(°C)'];
-            newItem['lluvia_hora'] = item['Lluvia hora(mm)'];
-            newItem['lluvia_24_horas'] = item['Lluvia 24 horas(mm)'];
-            newItem['lluvia_semana'] = item['Lluvia semana(mm)'];
-            newItem['lluvia_mes'] = item['Lluvia mes(mm)'];
-            newItem['lluvia_total'] = item['Lluvia Total(mm)'];
-
-// data from MODDatos estacion Calahuancane
-      //    newItem['yyyy'] = item['YYYY'];
-      //    newItem['mm'] = item['MM'];
-      //    newItem['dd'] = item['DD'];
-     //     newItem['time'] = item['Time'];
-      //    newItem['hi_temp'] = item['Hi_Temp'];
-       //   newItem['low_temp'] = item['Low_Temp'];
-        //  newItem['wind_cod'] =  item['Wind_Cod'];
-         // newItem['wind_run'] = item['Wind_Run'];
-         // newItem['hi_speed'] = item['Hi_Speed'];
-         // newItem['hi_dir'] = item['Hi_Dir'];
-         // newItem['wind_cod_dom'] = item['Wind_Cod_Dom'];
-         // newItem['wind_chill'] = item['Wind_Chill'];
-        //  newItem['index_heat'] = item['Heat_Index'];
-        //    newItem['index_thw'] = item['THW_Index'];
-        //    newItem['index_thsw'] = item['THSW_Index'];
-        //    newItem['presion_relativa'] = item['Bar'];
-        //    newItem['rain'] = item['Rain'];
-        //    newItem['lluvia_hora'] = item['Rain_Rate'];
-        //    newItem['solar_rad'] = item['Solar_Rad.'];
-        //    newItem['solar_energy'] = item['Solar_Energy'];
-        //    newItem['radsolar_max'] = item['Hi_Solar_Rad.'];
-        //    newItem['uv_index'] = item['UV_Index'];
-        //    newItem['uv_dose'] = item['UV_Dose'];
-        //    newItem['uv_max'] = item['Hi_UV'];
-        //    newItem['heat_days_d'] = item['Heat_D-D'];
-        //    newItem['cool_days_d'] = item['Cool_D-D'];
-        //    newItem['in_dew'] = item['In_Dew'];
-        //    newItem['in_heat'] = item['In_Heat'];
-        //    newItem['in_emc'] = item['In_EMC'];
-        //    newItem['in_air_density'] = item['In_Air_Density'];
-        //    newItem['evapotran'] = item['ET'];
-        //    newItem['soil_1_moist'] = item['Soil_1_Moist.'];
-        //    newItem['soil_2_moist'] = item['Soil_2_Moist.']; 
-        //    newItem['leaf_wet1'] = item['Leaf_Wet_1'];
-        //    newItem['wind_samp']=item['Wind_Samp'];
-        //    newItem['wind_tx'] = item['Wind_Tx'];
-        //    newItem['iss_recept'] = item['ISS_Recept'];
-        //    newItem['intervalo'] = item['Arc._Int.'];
-        //    newItem['temperatura_interna'] = item['In_Temp'];
-        //    newItem['humedad_interna'] = item['In_Hum'];
-        //    newItem['direccion_del_viento'] = item['Wind_Dir'];
-        //    newItem['velocidad_viento'] =item['Wind_Speed'];
-        //    newItem['punto_rocio'] = item['Dew_Pt.'];
-        //    newItem['humedad_externa'] = item['Out_Hum'];
-        //    newItem['temperatura_externa'] = item['Temp_Out'];
-       
+    // console.log("parsed data is ", JSON.stringify(parsedData) )
 
 
+    // con.connect(function(err){
 
 
+    //     if (err) throw err;
+    //     //Select all customers and return the result object:
+    //     con.query("INSERT INTO `chinas-davis` SET ?", parsedData, function (err, result, fields){
+    //     if (err) throw err;
+    //     console.log(result);
+    //     });
+    // });
 
-
-
-            if (err) throw err;
-            //Select all customers and return the result object:
-            connection.query("INSERT INTO `chinas-davis` SET ?;", newItem, function (err, result, fields){
-                if (err) throw err;
-                console.log(result);
-            })
-
-
-        })
-
-    })
- 
 }
 
 
 main();
 
+async function insertToTable(connection, newItem,callback){
+    //Select all customers and return the result object:
+    connection.query("INSERT INTO `chinas-davis` SET ?;", newItem, function (err, result, fields){
+        if (err) {
+            console.log("err",err);
+        }
+        else {
+            callback(null,result);
+        }
+    });
+}
 
+function processResult(err,result){
+    console.log(result)
+}
 //*** Stuff below here is only for when we want to run this on a server ***//
 //
 //
